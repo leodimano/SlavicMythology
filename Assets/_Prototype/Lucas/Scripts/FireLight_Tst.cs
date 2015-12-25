5a965b52129e73b5c656a1cc46d038057b40c55d
LucasGarcia12 <luckymorttadella@gmail.com>
2015-12-25 17:14:12 -0200

    Cenário Lucas - Area de testes

---
+++ Assets/_Prototype/Lucas/Scripts/FireLight_Tst.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireLight_Tst : MonoBehaviour {
+	
+	public float minIntensity;
+	public float maxIntensity;
+	Light LightRange;
+	float random;
+
+
+	// Use this for initialization
+	void Start () {
+		LightRange = GetComponent<Light> ();
+		random = Random.Range (0.0f, 3.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float noise = Mathf.PerlinNoise(random, Time.time);
+		LightRange.intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
+
+
+	}
+}
---
+++ Assets/_Prototype/Lucas/Scripts/Tst_Hud.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tst_Hud : MonoBehaviour {
+
+
+	void OnCollisionEnter (Collision Colisao)
+	{
+		if (Colisao.gameObject.tag == "Player") {
+			print ("Oh Você me tocou!");
+			Destroy (gameObject);
+
+		}
+
+	}
+}
